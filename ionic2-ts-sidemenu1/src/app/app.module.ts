590eb11f9a4394236bbd592525fe0f95b9c97325
eejai42 <start@anabstractlevel.com>
2017-12-15 18:50:38 -0600

    Added Rest API to the list of modules included (by default).

--- ionic2-ts-sidemenu1/src/app/app.module.ts
+++ ionic2-ts-sidemenu1/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { components } from './app.modules-base';
+import { RestAPIService } from '../providers/rest-api-service';
 
 components.push(MyApp);
 
@@ -12,6 +13,6 @@ components.push(MyApp);
   ],
   bootstrap: [IonicApp],
   entryComponents: components,
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, RestAPIService]
 })
 export class AppModule {}
